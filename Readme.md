582596e1dd4bd4a6371a0b6e4b285ce6ef3b04ef
yusufbek-murodov <yusufbekiqbolovich@gmail.com>
2024-02-18 12:42:02 +0500

    first commit from Yusufbek

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Group project
+## Car store
+### Members: Yusufbek, Ilyoskhoja, Xusniddin
+
+
+```java
+public class Main{
+    public static void main(String args[]){
+        System.out.println("hello everyone");
+    }
+}
+```
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello everyone");
+        System.out.println("car store project");
+        System.out.println("members: Yusufbek, Ilyoskhoja, Xusniddi");
+    }
+}
