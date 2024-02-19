43f02adf1f990fec18ee6151e42acd7ff4157fcc
yusufbek-murodov <yusufbekiqbolovich@gmail.com>
2024-02-19 11:42:38 +0500

    updated

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         System.out.println("car store project");
+        System.out.println("salom");
     }
 }
